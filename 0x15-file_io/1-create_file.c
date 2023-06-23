d68acaf8974352f80560fa822918d5df4eba90c6
Bettyje <bettyjelagat156@gmail.com>
2023-06-23 05:01:20 -0700

    adding scripts

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int xy;
+	int ax;
 	int nletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	xy = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	ax = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (xy == -1)
+	if (ax == -1)
 		return (-1);
 
 	if (!text_content)
@@ -27,12 +27,12 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(xy, text_content, nletters);
+	rwr = write(ax, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
 
-	close(xy);
+	close(ax);
 
 	return (1);
 }
